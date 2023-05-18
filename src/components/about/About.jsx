cdb236afe0e10028fea5ac6e99f46b9aa34f8232
Igor Silva <igor.silva@philips.com>
2023-05-18 13:55:26 -0300

    more adjustments of css

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,30 +12,30 @@ const About = () => {
       </div>
       <div className="a-right">
         <h1 className="a-title">About Me</h1>
-        <p className="a-sub">
-          I'm a Systems Analysis and Development Technologist
-        </p>
-
         <p className="a-sub">Soft Skills</p>
-          <ul className="a-desc">
-            <li>Effective communication</li>
-            <li>Teamwork</li>
-            <li>Critical thinking</li>
-            <li>Adaptability</li>
-            <li>Problem solving</li>
-            <li>Time management</li>
-            <li>Empathy</li>
-            <li>Leadership</li>
-            <li>Strategic thinking</li>
-            <li>Continuous learning</li>
-          </ul>
-
+        <ul className="a-desc">
+          <li>Effective Communication</li>
+          <li>Teamwork</li>
+          <li>Critical Thinking</li>
+          <li>Adaptability</li>
+          <li>Problem Solving</li>
+          <li>Time Management</li>
+          <li>Empathy</li>
+          <li>Leadership</li>
+          <li>Strategic Thinking</li>
+          <li>Continuous learning</li>
+        </ul>
         <p className="a-sub">Hard Skills</p>
-          <ul className="a-desc">
-            <li>Fatec Indaiatuba - Graduate Education, Systems Analysis and Development</li>
-            <li>SENAI "Prof. Dr. Euryclides de Jesus Zerbini" - Technical Education, Computer Science Technician</li>
-            <li>Rockfeller Blumenau - Extracurricular Education, English</li>
-          </ul>
+        <ul className="a-desc">
+          <li>Programming Languages</li>
+          <li>Algorithms and Data Structures</li>
+          <li>Web Development</li>
+          <li>Database Management</li>
+          <li>Debugging and Testing</li>
+          <li>Version Control</li>
+          <li>Mobile Development</li>
+          <li>Problem Solving</li>
+        </ul>
       </div>
     </div>
   );
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,5 @@
 import "./contact.css";
-import Phone from "../../img/phone.png";
 import Email from "../../img/email.png";
-import Address from "../../img/address.png";
 import { useContext, useRef, useState } from "react";
 import emailjs from "emailjs-com";
 import { ThemeContext } from "../../context";
@@ -39,16 +37,8 @@ const Contact = () => {
           <h1 className="c-title">Let's discuss your project</h1>
           <div className="c-info">
             <div className="c-info-item">
-              <img src={Phone} alt="" className="c-icon" />
-              +55 (19) 9 9727-5487
-            </div>
-            <div className="c-info-item">
               <img src={Email} alt="" className="c-icon" />
               igor.queiros@outlook.com
-            </div>
-            <div className="c-info-item">
-              <img src={Address} alt="" className="c-icon" />
-              Indaiatuba, SP - Brasil
             </div>
           </div>
         </div>
