4bb5b6ee64df72552efba0de3232f7e561a0b258
Igor Silva <igor.silva@philips.com>
2023-11-13 12:22:37 -0300

    Css changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
+    <div className="light">
     <BrowserRouter>
         <Navbar />
           <Routes>
