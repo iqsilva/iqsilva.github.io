e6b0258c2af039068a348f665f4b1fd6535155c6
Igor Silva <igor.silva@philips.com>
2023-11-16 17:57:58 -0300

    removed apollo

--- src/pages/productList/ProductList.jsx
+++ src/pages/productList/ProductList.jsx
@@ -2,62 +2,44 @@ import "./productList.css";
 import Product from "../../components/product/Product";
 import React, { useState, useEffect } from "react";
 import Loader from "../../components/loader/Loader";
-import { ApolloClient, createHttpLink, InMemoryCache, gql } from "@apollo/client";
-import { setContext } from '@apollo/client/link/context';
-
-const httpLink = createHttpLink({
-  uri: 'https://api.github.com/graphql',
-});
 
 const ProductList = () => {
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(false);
-  const token = import.meta.env.VITE_GITHUB_ACCESS_TOKEN
-
-  const authLink = setContext((_, { headers }) => {
-    return {
-      headers: {
-        ...headers,
-        authorization: `Bearer ${token}`,
-      }
-    }
-  });
-
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
-  });
 
   const getRepos = async () => {
     setLoading(true);
-    const { data } = await client.query({
-      query: gql`
-        {
-          user(login: "iqsilva") {
-            pinnedItems(first: 6, types: [REPOSITORY]) {
-              totalCount
-              edges {
-                node {
-                  ... on Repository {
-                    id
-                    name
-                    url
-                    description
-                    openGraphImageUrl
+    const response = await fetch('https://api.github.com/graphql', {
+      method: 'post',
+      headers: {
+        Authorization: 'key','Content-Type': 'application/json',
+      },
+        body: JSON.stringify({
+            query:  `{
+                user(login: "iqsilva") {
+                  pinnedItems(first: 6, types: REPOSITORY) {
+                    nodes {
+                      ... on RepositoryInfo {
+                        id
+                        name
+                        url
+                        description
+                        openGraphImageUrl
+                      }
+                    }
                   }
                 }
-              }
-            }
-          }
-        }
-      `
-    });
-    const { user } = data;
-    const pinnedItems = user.pinnedItems.edges.map(edge => edge.node);
-    setTimeout(() => {
-      setRepos(pinnedItems);
-      setLoading(false);
-    }, 3000);
+              }`
+            })
+          });
+          const result = await response.json();
+          result.data.user.pinnedItems.nodes.map((item) => {
+            repos.push(item);
+          });
+          setTimeout(() => {
+            setRepos(repos);
+            setLoading(false);
+          }, 3000);
   };
 
   useEffect(() => {
