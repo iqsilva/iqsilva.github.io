451029065dd0d1930478eedce3f9162eee5198fb
Igor Silva <igor.silva@philips.com>
2023-12-01 15:12:14 -0300

    styles(app): css changes

--- src/components/alert/Alert.jsx
+++ src/components/alert/Alert.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import "./alert.css";
 
 export default function Alert({ children, type, message }) {
   Alert.propTypes = {
